0a5815f31762b2118944cbf337f6e9b92f7557a8
Prity Kumari <120323872+pritykumari1910@users.noreply.github.com>
2024-12-09 09:11:18 +0530

    firebase.js

--- swm-eGarbage-main/src/components/firebase.js
+++ swm-eGarbage-main/src/components/firebase.js
@@ -10,14 +10,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  databaseURL: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: ,
-  measurementId: ,
+ apiKey: "AIzaSyBloghfhFwhEc4r9qdOTel7CnJ6iz7m9k0",
+  authDomain: "smart-bin-b30d2.firebaseapp.com",
+  databaseURL: "https://smart-bin-b30d2-default-rtdb.firebaseio.com",
+  projectId: "smart-bin-b30d2",
+  storageBucket: "smart-bin-b30d2.firebasestorage.app",
+  messagingSenderId: "23174065932",
+  appId: "1:23174065932:web:2c546fe56c7d9bcc412329",
+  measurementId: "G-ED76D79XWV"
 };
 
 // Initialize Firebase
